47fe42a34299dae9a71ec58f8458e0ab234ad35a
Carlos Ramirez <carlos@extrasistemas.com>
2020-08-02 23:04:25 -0500

    added files .js, .css and index.html

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "./src/js/app.js"),
+  },
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "./public/js/"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          fix: true,
+        },
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: path.resolve(__dirname,'./public/css/style.css'),
+    }),
+  ],
+};
