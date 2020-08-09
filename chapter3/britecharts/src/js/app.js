94cd42dabfdcb5cef081ca02601eae061f1191c9
Carlos Ramirez <carlos@extrasistemas.com>
2020-08-08 23:28:56 -0500

    changed requiere by import to css

--- chapter3/britecharts/src/js/app.js
+++ chapter3/britecharts/src/js/app.js
@@ -1,7 +1,7 @@
 import { letterFrequency } from "../../public/mocks/data.js";
 import { bar } from "britecharts";
 import { select } from "d3-selection";
-require("britecharts/dist/css/britecharts.css");
+import "britecharts/dist/css/britecharts.css";
 
 const britechartsBar = () => {
   const barChart = bar();
