3ba1c5bc81f1973850f9ed3fc82d8d9b68cd6d80
Carlos Ramirez <carlos@extrasistemas.com>
2020-08-08 22:55:57 -0500

    added funtion by chart

--- chapter3/britecharts/src/js/app.js
+++ chapter3/britecharts/src/js/app.js
@@ -1,7 +1,17 @@
 import { letterFrequency } from "../../public/mocks/data.js";
-import {bar} from 'britecharts';
-import {select} from 'd3-selection';
+import { bar } from "britecharts";
+import { select } from "d3-selection";
 require("britecharts/dist/css/britecharts.css");
 
+const britechartsBar = () => {
+  const barChart = bar();
+  const barContainer = select("#chart");
+
+  barChart.valueLabel("frequency").nameLabel("letter").width(900).height(600);
+
+  barContainer.datum(letterFrequency).call(barChart);
+};
+
 (function () {
+    britechartsBar();
 })();
