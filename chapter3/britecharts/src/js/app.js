d7696d87d53ddc82d9b31fa7cf7b4d6839656cd9
Carlos Ramirez <carlos@extrasistemas.com>
2020-08-08 22:36:11 -0500

    initial project britecharts

---
+++ chapter3/britecharts/src/js/app.js
@@ -0,0 +1,7 @@
+import { letterFrequency } from "../../public/mocks/data.js";
+import {bar} from 'britecharts';
+import {select} from 'd3-selection';
+require("britecharts/dist/css/britecharts.css");
+
+(function () {
+})();
